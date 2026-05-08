74440c471499959dea69976344ba2ad252aabb0d
Solit Computer <solitcomputer.03@gmail.com>
2026-05-08 10:26:35 +0700

    update katalog

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://yobgxhgmujzxsabqlpvp.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvYmd4aGdtdWp6eHNhYnFscHZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMTkwOTUsImV4cCI6MjA5MzU5NTA5NX0.aHfRzPvn8n2zp9XASUxX46CHdR3vENGFPV4d9mFxzLQ";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
